5e869d45177a57bcfe1451a83f3c4b9cb0a56c57
hjchoi <juhye987@naver.com>
2024-07-15 19:04:23 +0900

    [MODIFY] dataset.py

--- abstract_structure/dataset/dataset.py
+++ abstract_structure/dataset/dataset.py
@@ -27,29 +27,68 @@ from abstract_structure.config.train_config import Config
 class TokenizedCorpus(Dataset):
     def __init__(self, config, mode='train'):
         #
-        self.config = config
+        self.config = Config(config)
         self.mode = mode
-        self.saved_vocab = config.dataset_info['vocab_path']
-        self.saved_model = config.dataset_info['model_path']
         #
-        assert self.saved_model is not None, "No kowiki_small.model"
-        assert self.saved_vocab is not None, "No kowiki_small.vocab"
+        self.saved_vocab = config.dataset_info['vocab_path']
+        self.vocab = self.make_subtokenizer()
         #
         self.corpus_path = config.dataset_info['corpus_path']
+        print("Data Loading...")
         self.corpus_file = sorted(glob.glob(self.corpus_path + '/*.json'))
-
+        print("Loading complete")
 
     def __len__(self):
-        assert len(self.corpus_file) != 0 , "corpus_file is empty"
+        assert len(self.corpus_file) != 0, "corpus_file is empty"
         return len(self.corpus_file)
 
     def __getitem__(self, item):
         with open(self.corpus_file[item], 'r') as f:
             self.tokens = json.load(f)
+            self.tokens['tokens'].append(0)
         return (torch.tensor(self.tokens['tokens'][:-1]),
                 torch.tensor(self.tokens['tokens'][1:]),
                 torch.tensor(item))
 
+    def make_subtokenizer(self):
+        if self.mode == "train":
+            print("Loading subtokenizer-- training")
+            if self.saved_vocab is not None and os.path.exists(self.saved_vocab):
+                vocab = spm.SentencePieceProcessor()
+                vocab.Load(self.saved_vocab)
+                #
+                assert vocab.vocab_size() - 7 == self.config.dataset_info[
+                    'n_vocab'], "the size of vocabulary is not the same..."
+                #
+                print("[SubTokenizer] Loading Complete...")
+            else:
+                spm.SentencePieceTrainer.Train(
+                    f"--input={self.config.dataset_info['train_corpus']} --model_prefix={config.dataset_info['dataset_name']} --vocab_size={config.dataset_info['n_vocab'] + 7}" +
+                    " --model_type=bpe" +
+                    " --max_sentence_length=999999" +
+                    " --pad_id=0 --pad_piece=[PAD]" +
+                    " --unk_id=1 --unk_piece=[UNK]" +
+                    " --bos_id=2 --bos_piece=[BOS]" +
+                    " --eos_id=3 --eos_piece=[EOS]" +
+                    " --user_defined_symbols=[SEP],[CLS],[MASK]")
+                #
+                vocab = spm.SentencePieceProcessor()
+                vocab.Load(config.dataset_info['dataset_name'] + ".model")
+                #
+                print("[SubTokenizer] Training Completed...")
+        else:
+            print("Loading subtokenizer--evaluating")
+            assert self.saved_vocab is not None and os.path.exists(self.saved_vocab), "No kowiki_small.vocab"
+
+            vocab = spm.SentencePieceProcessor()
+            vocab.Load(self.saved_vocab)
+            #
+            assert self.vocab.vocab_size() -7 == self.config.dataset_info['n_vocab'], \
+                "the size of vocabulary is not the same..."
+            #
+            print("[SubTokenizer] Loading Completed....")
+
+        return vocab
 
     @staticmethod
     def collate_fn(data):
@@ -58,12 +97,15 @@ class TokenizedCorpus(Dataset):
         inputs = torch.nn.utils.rnn.pad_sequence(inputs, batch_first=True, padding_value=0)
         outputs = torch.nn.utils.rnn.pad_sequence(outputs, batch_first=True, padding_value=0)
         #
-        batch= [
+        batch = [
             inputs,
             outputs,
             torch.stack(item, dim=0)
         ]
         return batch
+
+
+
 
 if __name__ == '__main__':
     from abstract_structure.config.train_config import get_config_dict
